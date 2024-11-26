b83badbe438eca1bca64ae85fde2a7eed93000f4
Василий Титов <v.titov@faberlic.com>
2024-11-26 22:35:09 +0400

    Fix typos in project name from "invocies" to "invoices"
    Corrected all occurrences of "invocies-backoffice-app" to "invoices-backoffice-app" in various files including package.json, angular.json, styles.scss, app.component files, and index.html. This fixes typo issues and aligns the project name consistently across the codebase.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'invocies-backoffice-app' title`, () => {
+  it(`should have the 'invoices-backoffice-app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('invocies-backoffice-app');
+    expect(app.title).toEqual('invoices-backoffice-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, invocies-backoffice-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, invoices-backoffice-app');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LayoutComponent],
+  imports: [LayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'invocies-backoffice-app';
+  title = 'invoices-backoffice-app';
 }
