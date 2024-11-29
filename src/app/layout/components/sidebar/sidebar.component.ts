a03d756b61a375aa2040302b1db9211a088e894a
Василий Титов <v.titov@faberlic.com>
2024-11-29 22:36:51 +0400

    86c156g05: Enhance layout and sidebar with Bootstrap integration
    Refactor sidebar with Bootstrap and Angular animations for responsive control and improved user experience. Incorporate a new layout structure and add a header with user profile features. The design updates also introduce a consistent look using SCSS and Bootstrap styles across the application.

--- src/app/layout/components/sidebar/sidebar.component.ts
+++ src/app/layout/components/sidebar/sidebar.component.ts
@@ -1,12 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  animate,
+  animateChild,
+  group,
+  query,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
   imports: [],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrl: './sidebar.component.scss',
+  animations: [
+    trigger('toggleSidebar', [
+      state(
+        'expanded',
+        style({
+          width: '15rem',
+        }),
+      ),
+      state(
+        'collapsed',
+        style({
+          width: '3.75rem',
+        }),
+      ),
+      transition(
+        'expanded <=> collapsed',
+        group([query('@*', animateChild()), animate('300ms ease-in-out')]),
+      ),
+    ]),
+    trigger('toggleText', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('500ms ease-in', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [animate('200ms ease-out', style({ opacity: 0 }))]),
+    ]),
+    trigger('rotateArrow', [
+      state(
+        'expanded',
+        style({
+          transform: 'rotate(180deg)',
+        }),
+      ),
+      state(
+        'collapsed',
+        style({
+          transform: '*',
+        }),
+      ),
+      transition('expanded <=> collapsed', [animate('300ms ease-in-out')]),
+    ]),
+  ],
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
+  isCompact: boolean = false;
 
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  onToggle(): void {
+    this.isCompact = !this.isCompact;
+  }
+
+  ngOnInit(): void {
+    this.breakpointObserver.observe(['(max-width: 1023.99px)']).subscribe((result) => {
+      this.isCompact = result.matches;
+    });
+  }
 }
