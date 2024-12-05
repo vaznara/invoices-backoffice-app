6721603974870dd5e9bff990b0df0cce11f2e4f3
Василий Титов <v.titov@faberlic.com>
2024-12-05 23:21:11 +0400

    Add AlertComponent with HTML, TS, and test setup
    This commit introduces the AlertComponent with basic structure and functionality, including a dismissible alert feature. The TypeScript file defines component properties and behaviors, while the HTML implements its visual elements. Additionally, a test spec is set up to ensure the component initializes correctly.

---
+++ src/app/shared/components/alert/alert.component.ts
@@ -0,0 +1,45 @@
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  Output,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
+import { BSColor } from '../../interfaces/ui';
+import { NgClass } from '@angular/common';
+import { AlertService } from '../../services/alert.service';
+import { Subject, takeUntil } from 'rxjs';
+
+@Component({
+  selector: 'app-alert',
+  standalone: true,
+  imports: [NgClass],
+  templateUrl: './alert.component.html',
+  styleUrl: './alert.component.scss',
+})
+export class AlertComponent {
+  @Input() id!: string;
+  @Input() color: BSColor = 'primary';
+  @Input() text?: string;
+  @Input() isDismissible: boolean = true;
+
+  @Output() alertHide: EventEmitter<void> = new EventEmitter();
+
+  @ViewChild('alert') alertElement?: ElementRef<HTMLDivElement>;
+
+  constructor(private renderer: Renderer2) {}
+
+  onClose(): void {
+    this.renderer.removeClass(this.alertElement?.nativeElement, 'show');
+    this.renderer.removeClass(this.alertElement?.nativeElement, 'fade');
+    this.alertHide.emit();
+  }
+
+  get alertColor(): string {
+    return `alert-${this.color}`;
+  }
+}
